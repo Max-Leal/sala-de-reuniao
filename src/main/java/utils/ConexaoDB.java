7e038271fc301d84854910b65fb6a42b099ee4d0
Max-leal <Max13leal@gmail.com>
2025-06-16 08:40:39 -0300

    Adicionado conexão com JDBC

---
+++ src/main/java/utils/ConexaoDB.java
@@ -0,0 +1,19 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConexaoDB {
+
+	public static Connection getConexao() throws SQLException {
+		String url = "jdbc:mysql://localhost:3306/deepblue";
+		String usuario = "root";
+		String senha = "1234567";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			throw new SQLException("Driver MySQL não encontrado.", e);
+		}
+		return java.sql.DriverManager.getConnection(url, usuario, senha);
+	}
+}
